6e390e42324d2d9b471492d8fabe7018d8f09e07
gvega001 <gvega001@gmail.com>
2021-01-28 00:00:34 -0800

    Initial commit

---
+++ src/online/dev4you/Main.java
@@ -0,0 +1,23 @@
+package online.dev4you;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("online.dev4you.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
