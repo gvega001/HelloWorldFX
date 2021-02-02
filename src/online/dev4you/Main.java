fbc3d02c532d8e0deeec08ad93c7eb5ea14e834b
gvega001 <gvega001@gmail.com>
2021-02-01 21:24:15 -0800

    created a padding to set the insets

--- src/online/dev4you/Main.java
+++ src/online/dev4you/Main.java
@@ -6,12 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("online.dev4you.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Welcome to FX!");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
